5fcb324d9f0a626ebd77b5ae5f7b8c6809f88481
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-04 20:11:04 +0200

    dataloader is updated with code of load dataset

---
+++ src/dataloader.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+class Loader():
+    def __init__(self, dataset = None, test_size = 0.20, shuffle = True, random_state = 42, preprocess=False):
+        self.dataset = dataset
+        self.test_size = test_size
+        self.random_state = random_state
+        self.shuffle = shuffle
+        self.preprocess = preprocess
+        
+    def load_dataset(self):
+        dataset = pd.read_csv(self.dataset)
+        
+        if isinstance(dataset, pd.pandas.DataFrame):
+            
+            if not self.preprocess:
+            
+                dataset = dataset.rename(columns={dataset.columns[-1]: "sequence" , dataset.columns[0]: "labels"})
+                
+                if "S10" in dataset.columns:
+                    dataset.drop(["S10"], axis = 1, inplace=True)
+                else:
+                    Exception("S10 Feature is not available in the dataset".capitalize())
+                
+            dataset["labels"] = dataset["labels"].map({"+": 1, "-": 0}).astype("int")
+            dataset["sequence"] = dataset["sequence"].str.replace("\t", "").str.upper()
+            
+            print(dataset.head(2))
+        
+        else:
+            ValueError("Dataset should be in the format of pandas dataFrame".capitalize())
+    
+    
+if __name__ == "__main__":
+    loader = Loader(
+        dataset="./data/raw/promoters.data",
+        test_size=0.25,
+        shuffle=True,
+        random_state=42,
+        preprocess=False
+    )
+    
+    loader.load_dataset()
