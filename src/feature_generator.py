e7ea7085779c5edfc81813407c963d8689326aa0
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-06-05 14:25:11 +0200

    tri nucleosides in GAP -3 is added

--- src/feature_generator.py
+++ src/feature_generator.py
@@ -99,7 +99,7 @@ class FeatureGenerator():
                     self.value = []
                     self.sequence = dataset.loc[index, "sequence"]
                     
-                    for idx in range(len(self.sequence) - 2 - 2): # (1: GAP, 1: tri -->tri)
+                    for idx in range(len(self.sequence) - 2 - 2): # (2: GAP, 1: tri -->tri)
                         for _, nucleosides in enumerate(self.nucleosides):
                             seq = self.sequence[idx] + self.sequence[idx + 3] + self.sequence[idx + 4] # A__AA
                                 
@@ -111,6 +111,23 @@ class FeatureGenerator():
                                 
                     for idx, val in enumerate(self.value):
                         dataset.loc[index, f"2_GAP_KMer_tri_{idx}"]=val
+                        
+                for index in tqdm(range(dataset.shape[0])):
+                    self.value = []
+                    self.sequence = dataset.loc[index, "sequence"]
+                    
+                    for idx in range(len(self.sequence) - 3 - 2): # (3: GAP, 1: tri -->tri)
+                        for _, nucleosides in enumerate(self.nucleosides):
+                            seq = self.sequence[idx] + self.sequence[idx + 4] + self.sequence[idx + 5] # A___AA
+                                
+                            if seq == nucleosides:
+                                self.value.append(1)
+                                
+                            else:
+                                self.value.append(0)               
+                                
+                    for idx, val in enumerate(self.value):
+                        dataset.loc[index, f"3_GAP_KMer_tri_{idx}"]=val
                         
                         
                 return dataset
