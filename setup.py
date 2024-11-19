50716dc14c802ba6aa8c42b99ca6046860eaad03
Atikul Islam Sajib <112280249+atikul-islam-sajib@users.noreply.github.com>
2024-11-19 10:21:37 +0100

    setup.py file is added and updated

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="promoter-bioinformatics",
+    version="0.0.1",
+    description="DNA classification is the process of categorizing DNA sequences into specific groups or types based on their genetic similarities, differences, or functional characteristics.",
+    author="Atikul Islam Sajib",
+    author_email="atikulislamsajib137@gmail.com",
+    url="https://github.com/atikul-islam-sajib/promoter-bioinformatics.git",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="DNA-Classification, machine-learning",
+    project_urls={
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/promoter-bioinformatics/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/promoter-bioinformatics",
+        "Source Code": "https://github.com/atikul-islam-sajib/promoter-bioinformatics",
+    },
+)
